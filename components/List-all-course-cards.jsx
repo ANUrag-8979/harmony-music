28c5776a5068f1932f540b39151c9df6c87d1d9a
ANUrag-8979 <147045106+ANUrag-8979@users.noreply.github.com>
2025-06-22 22:20:45 +0530

    debuged the imports of list-all-couse...

--- components/List-all-course-cards.jsx
+++ components/List-all-course-cards.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { ProductCard } from "./ProductCard";
+import { ProductCard } from "./productCard";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 export function ProductList() {
